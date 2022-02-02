d3a0b5b7e3b30b986c0e8ff849eff998dc45da20
Amarjit <jha.amarjit@gmail.com>
2022-02-02 20:31:54 +0530

    Code & Documentation commit

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-# react-capture-pin
-A cleaner and whole new different out of the box approach to capture pins or isolated key inputs
+# Capture Isolated Inputs
 
-It can be used in plain JavaScript project as well. Just look at the code, should be very easy to copy and past into your projects.
+A cleaner and whole new different out-of-the-box approach to capture PINs or isolated key inputs.
+
+It can be used with a plain JavaScript project as well. Just look at the code, should be very easy to leverage.
+
+`Although It discourages a blind copy/paste of the code. Should be analysed and used conciously.`
+
+## Why not use equivalent number of Input boxes?
+
+Not using input boxes makes thing much easier. Here are the issues that I faced with input boxes :
+
+- Unnecessary flexbility to users to navigate across the boxes.
+- Lag observed while moving the focus to next/previous sibling boxes.
+- Too many scenario handling resulting in much larger & complex code than this.
+
+[![Capture PIN](https://github.com/jhaspider/react-capture-pin/blob/main/public/capture_pin.png)]
--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CaptureInput from "./capture_input";
 
-const App = () => <h1>Hello World. This is Cool!</h1>;
+const App = () => {
+  return (
+    <div className="landing">
+      <div className="container">
+        <h1>Capture Isolated Inputs</h1>
+        <p>A cleaner and whole new different out-of-the-box approach to capture PINs or isolated key inputs.</p>
+        <p>It can be used with a plain JavaScript project as well. Just look at the code, should be very easy to leverage.</p>
+        <p style={{ color: "red" }}>Although It discourages a blind copy/paste of the code. Should be analysed and used conciously.</p>
+        <h2>Why not use equivalent number of Input boxes?</h2>
+        <p>Not using input boxes makes thing much easier. Here are the issues that I faced with input boxes :</p>
+        <ul>
+          <li>Unnecessary flexbility to users to navigate across the boxes.</li>
+          <li>Lag observed while moving the focus to next/previous sibling boxes.</li>
+          <li>Too many scenario handling resulting in much larger &amp; complex code than this.</li>
+        </ul>
+        <CaptureInput />
+      </div>
+    </div>
+  );
+};
 ReactDOM.render(<App />, document.getElementById("root"));
